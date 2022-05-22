939cce078dab922ed262263400583dc7196092d0
Ítalo Epifânio <italoepifaniols@gmail.com>
2022-05-21 21:11:11 -0300

    Add bhaskara

---
+++ tests/test_formula.cpp
@@ -0,0 +1,31 @@
+#include "../include/doctest.h"
+#include "../include/math/formula.inl"
+
+TEST_CASE("it can calculate delta")
+{
+    double a = 2;
+    double b = -6;
+    double c = 0;
+    double result = bhaskaraDelta(a, b, c);
+    CHECK_EQ(result, 36);
+}
+
+TEST_CASE("it can calculate bhaskara with two roots")
+{
+    double a = 2;
+    double b = -6;
+    double c = 0;
+    auto result = bhaskara(a, b, c);
+    CHECK_EQ(result.first, 0);
+    CHECK_EQ(result.second, 3);
+}
+
+TEST_CASE("it can calculate bhaskara with one root")
+{
+    double a = 1;
+    double b = -8;
+    double c = 16;
+    auto result = bhaskara(a, b, c);
+    CHECK_EQ(result.first, 4);
+    CHECK_EQ(result.second, 4);
+}
