14947103458b58801d1f3760efd8ec1d09b3a864
Epitácio Bessa da Silva <bessa.epitacio@gmail.com>
2022-06-06 11:44:20 -0300

    Ler xml da main e gerar arquivos de saída

--- drivers/main-driver.cpp
+++ drivers/main-driver.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../include/core/api.h"
+#include "../include/core/scene.h"
 #include "../include/core/rt3.h"
 #include <fstream>
 #include <sstream>
@@ -19,35 +20,8 @@ int main(int argc, char **argv)
             RunningOptions options(sceneFile);
             Api api(options);
             api.run();
-            
-            Background background = api.getBackground();
-            Camera * camera = api.getCamera();
-
-            auto w = camera->film.getXRes();
-            auto h = camera->film.getYRes();
-            std::cout << "Ray's Origin                         Ray's Direction\n";
-            for (int j = h - 1; j >= h-3; j--)
-            {
-                for (int i = 0; i < w; i+=700)
-                {
-                    std::cout << "i=" << i << " j=" << j;
-                    Ray r2 = camera->generate_ray(i, j);
-                    std::cout << std::endl;
-                    camera->print(r2);
-                    
-                }
-            }
-            for (int j = 2; j >= 0; j--)
-            {
-                for (int i = 0; i < w; i+=700)
-                {
-                    std::cout << "i=" << i << " j=" << j;
-                    Ray r2 = camera->generate_ray(i, j);
-                    std::cout << std::endl;
-                    camera->print(r2);
-                    
-                }
-            }
+            // Scene scene(api.getCamera(), api.getBackground(), api.getPrimitives());
+            // scene.render();
         }
         else
         {
